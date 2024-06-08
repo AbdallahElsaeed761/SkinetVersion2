2a26f7615581347149d70de9b8d0f01d6f005f9d
Abdallah Elsaeed <abdullahalsaeed36@gmail.com>
2024-06-08 12:55:41 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities.Products;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _storeDbContext;
+
+        public ProductsController(StoreDbContext storeDbContext)
+        {
+            this._storeDbContext = storeDbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> getProduct()
+        {
+            var products =await _storeDbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> getProductById(int id)
+        {
+            var product = await _storeDbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+    }
+}
---
+++ Infrastructure/Data/StoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Infrastructure.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+
+    
+}
